b8f1bda76c097a29704b55f3b41f0c01a5a1f8f9
Kristoffer Kittilsen <kristoffer-2000@hotmail.com>
2020-10-26 11:43:40 +0100

    Initial Commit

---
+++ Exam_PG5601/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam_PG5601
+//
+//  Created by Kristoffer Kittilsen on 26/10/2020.
+//  Copyright © 2020 Kristoffer Kittilsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Exam_PG5601Tests/Exam_PG5601Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Exam_PG5601Tests.swift
+//  Exam_PG5601Tests
+//
+//  Created by Kristoffer Kittilsen on 26/10/2020.
+//  Copyright © 2020 Kristoffer Kittilsen. All rights reserved.
+//
+
+import XCTest
+@testable import Exam_PG5601
+
+class Exam_PG5601Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
